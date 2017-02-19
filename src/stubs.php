e4a9fa3d9014e977a1b2b85e94f262136c057ccf
Cyrus Collier <web@cyruscollier.com>
2017-02-19 16:48:42 -0500

    only stub when running phpspec

--- src/stubs.php
+++ src/stubs.php
@@ -1,12 +1,14 @@
 <?php
 
 /* Mocked WP functions used in domain objects */
-function apply_filters($tag, $value) { return $value; }
-function do_action($tag) { }
-function get_the_excerpt($post) { return $post->post_excerpt; }
-function maybe_unserialize($value) { return $value; }
-function maybe_serialize($value) { return $value; }
-function sanitize_title($title) { return $title; }
+if (defined('PHPSPEC_VERSION')) {
+    function apply_filters($tag, $value) { return $value; }
+    function do_action($tag) { }
+    function get_the_excerpt($post) { return $post->post_excerpt; }
+    function maybe_unserialize($value) { return $value; }
+    function maybe_serialize($value) { return $value; }
+    function sanitize_title($title) { return $title; }
 
-/* Stubbed WP classes */
-class WP_Widget {}
+    /* Stubbed WP classes */
+    class WP_Widget {}
+}

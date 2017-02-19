7448832927ce89b25d1db14cae6465dcb72d62bf
Cyrus Collier <web@cyruscollier.com>
2017-02-19 16:00:45 -0500

    moved stubs to autoloader

--- bootstrap/unit.php
+++ bootstrap/unit.php
@@ -13,14 +13,3 @@ define('WP_CONTENT_DIR', $Util->getWPContentDirectory());
 
 require_once WP_PATH . '/src/wp-includes/class-wp-post.php';
 require_once WP_PATH . '/src/wp-includes/class-wp-post-type.php';
-
-/* Mocked WP functions used in domain objects */
-function apply_filters($tag, $value) { return $value; }
-function do_action($tag) { }
-function get_the_excerpt($post) { return $post->post_excerpt; }
-function maybe_unserialize($value) { return $value; }
-function maybe_serialize($value) { return $value; }
-function sanitize_title($title) { return $title; }
-
-/* Stubbed WP classes */
-class WP_Widget {}
